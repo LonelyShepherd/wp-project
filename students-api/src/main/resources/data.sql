191d3df53ff90ec6202eca3cf09e835e7faee2ec
Nikola Stojanovikj <nikolastojanovic.hj@hotmail.com>
2018-12-03 10:10:06 +0100

    Seed data

---
+++ students-api/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO studyPrograms (name) VALUES
+    ('KNI'),
+    ('MT')
+    ('PET')
+
+INSERT INTO students (index, name, lastName, studyProgram) VALUES
+    ("162010", "Nikola", "Stojanovikj", "3")
+    ("162010", "Pero", "Deformero", "1")
+    ("162010", "Test", "Testikj", "2")
+    ("162010", "Posleden", "Insert", "1")
